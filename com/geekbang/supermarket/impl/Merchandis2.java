31f564b3ac3ab9c03d85d2ed269b9adea55249cb
malingzhi58 <46583742+malingzhi58@users.noreply.github.com>
2020-04-08 12:05:45 +0800

    Add files via upload

---
+++ com/geekbang/supermarket/impl/Merchandis2.java
@@ -0,0 +1,46 @@
+package com.geekbang.supermarket.impl;
+
+import com.geekbang.supermarket.interfaces.Category;
+import com.geekbang.supermarket.interfaces.Merchandise;
+
+public class Merchandis2 implements Merchandise {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public double getSoldPrice() {
+        return 0;
+    }
+
+    @Override
+    public double getPurchasePrice() {
+        return 0;
+    }
+
+    @Override
+    public int buy(int count) {
+        return 0;
+    }
+
+    @Override
+    public void putBack(int count) {
+
+    }
+
+    @Override
+    public Category getCategory() {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public void describe() {
+
+    }
+}
