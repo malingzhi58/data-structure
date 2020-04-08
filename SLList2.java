880ae1740833cce90b981b8cd7becac3fe4d9d63
malingzhi58 <ma_lingzhi@126.com>
2020-04-08 18:36:56 +0800

    m

--- SLList2.java
+++ SLList2.java
@@ -1,44 +1,59 @@
-public class SLList2 {
-    // this is my practice
+public class SLList2<String2> {
+    /*
+     * IntNode
+     * initialize SLList
+     * method:addFirst,getFirst, addLast, size()
+     */
     public class IntNode2 {
-        public int item;
+        public String2 item;
         public IntNode2 next;
 
-        public IntNode2(int item, IntNode2 next) {
+        public IntNode2(String2 item, IntNode2 next) {
             this.item = item;
             this.next = next;
         }
     }
 
-    //antoehr thing
-    // add somthing new
     public IntNode2 first;
+    private int size;
 
-    public SLList2(int x) {
+    public SLList2(String2 x) {
         first = null;
         first = new IntNode2(x, null);
+        size =1;
     }
-    public void addfirst2(int y){
-        first = new IntNode2(y,first);
+
+    public void addfirst2(String2 y) {
+        first = new IntNode2(y, first);
+        size +=1;
     }
-    public int getfirst2() {
+
+    public String2 getfirst2() {
         return first.item;
     }
-    public int size_way1(IntNode2 first){
+
+    public int size_way1(IntNode2 first) {
         int size = 1;
-        if(first == null){
-            return size;
-        }else {
+        if (first != null) {
             while (first != null) {
                 first = first.next;
                 size += 1;
             }
-            return size;
         }
+//        if (first == null) {
+//            return size;
+//        } else {
+//            while (first != null) {
+//                first = first.next;
+//                size += 1;
+//            }
+        return size;
     }
-    public int size(){
+
+    public int size() {
         return size(first);
     }
+
     /*
     public void addLast(int x) {
         IntNode p = first;
@@ -49,18 +64,20 @@ public class SLList2 {
         }
         p.next = new IntNode(x, null);
     }*/
-    private static int size(IntNode2 p) {
+    private int size(IntNode2 p) {
         if (p.next == null) {
             return 1;
         }
         return 1 + size(p.next);
     }
+
     public static void main(String[] args) {
-        SLList2 L = new SLList2(1);
+        SLList2<Integer> L = new SLList2<Integer>(1);
         L.addfirst2(2);
         L.addfirst2(3);
         int x = L.getfirst2();
         int y = L.size();
         System.out.println(y);
+        System.out.println();
     }
 }
