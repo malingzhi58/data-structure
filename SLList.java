ef79fb1af9c17b558613b82e8e485fdccb6d040d
malingzhi58 <ma_lingzhi@126.com>
2020-04-08 19:12:25 +0800

    change

--- SLList.java
+++ SLList.java
@@ -1,65 +1,66 @@
+/** An SLList is a list of integers, which hides the terrible truth
+ * of the nakedness within. */
 public class SLList {
-    public class IntNode {
+    private static class IntNode {
         public int item;
         public IntNode next;
+
         public IntNode(int i, IntNode n) {
             item = i;
             next = n;
         }
+    }
 
-    private IntNode first;
-    private IntNode Sentinenal;
+    /* The first item (if it exists) is at sentinel.next. */
+    private IntNode sentinel;
     private int size;
 
+    /** Creates an empty SLList. */
     public SLList() {
-        first = null;
+        sentinel = new IntNode(63, null);
         size = 0;
     }
 
     public SLList(int x) {
-        first = new IntNode(x, null);
+        sentinel = new IntNode(63, null);
+        sentinel.next = new IntNode(x, null);
         size = 1;
     }
 
-    /** Adds an item to the front of the list. */
+    /** Adds x to the front of the list. */
     public void addFirst(int x) {
-        first = new IntNode(x, first);
-        size += 1;
+        sentinel.next = new IntNode(x, sentinel.next);
+        size = size + 1;
     }
 
-    /** Retrieves the front item from the list. */
+    /** Returns the first item in the list. */
     public int getFirst() {
-        return first.item;
+        return sentinel.next.item;
     }
 
-    /** Returns the number of items in the list. */
+    /** Adds x to the end of the list. */
+    public void addLast(int x) {
+        size = size + 1;
+
+        IntNode p = sentinel;
+
+        /* Advance p to the end of the list. */
+        while (p.next != null) {
+            p = p.next;
+        }
+
+        p.next = new IntNode(x, null);
+    }
+
+    /** Returns the size of the list. */
     public int size() {
         return size;
     }
 
-    /** Adds an item to the end of the list. */
-    public void addLast(int x) {
-        if(first ==null){
-            first = new IntNode(x,null);
-            size +=1;
-        }else {
-            IntNode p = first;
-
-            /* Advance p to the end of the list. */
-            while (p.next != null) {
-                p = p.next;
-            }
-            p.next = new IntNode(x, null);
-            size +=1;
-        }
-    }
-
-    /** Crashes when you call addLast on an empty SLList. Fix it. */
     public static void main(String[] args) {
-        SLList x = new SLList();
-//        System.out.println(x.first);
-        x.addLast(5);
-//        x.addFirst(0);
-        System.out.println(x.size());
+        /* Creates a list of one integer, namely 10 */
+        SLList L = new SLList();
+        L.addLast(20);
+        System.out.println(L.size());
     }
 }
